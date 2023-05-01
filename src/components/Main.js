efcf66d0f4999e26b1942353d7aff5d7c1d28054
Imbeshat <md.imbeshat@gmail.com>
2023-05-02 02:09:14 +0530

    minor edits

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,8 +7,8 @@ const Main = () => {
 	return (
 		<main className="main-content">
 			<Header />
-			<div className="flex flex-col justify-center items-center text-white pt-8 md:pt-10">
-				<h1 className="text-5xl md:text-6xl lg:text-7xl text-center py-4">Find Your Book</h1>
+			<div className="flex flex-col justify-center items-center text-white mt-6 md:mt-10">
+				<h1 className="text-5xl md:text-6xl lg:text-7xl text-center mb-4">Find Your Book</h1>
 				<p className="w-[80%] md:w-[600px]">
 					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis velit debitis quod ipsam accusamus voluptatum iusto aspernatur fuga suscipit
 					officiis rerum corrupti, dolorum hic sequi eius iste eum molestias, ipsum error soluta maiores perferendis sed mollitia amet. Illum impedit
