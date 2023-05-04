05a90823c30320ffd450b33307b816e7d49c2050
Imbeshat <md.imbeshat@gmail.com>
2023-05-05 01:30:30 +0530

    minor change

--- src/components/BookItem.js
+++ src/components/BookItem.js
@@ -9,7 +9,7 @@ const BookItem = ({ data }) => {
 						return (
 							<div key={item.id} className="w-auto rounded-2xl bg-slate-800 p-2">
 								<img src={item.volumeInfo.imageLinks.thumbnail} alt="meal" className="rounded-2xl" />
-								<h3 className="text-white text-lg">{item.volumeInfo.thumbnail}</h3>
+								<h3 className="text-white text-lg">{item.volumeInfo.title}</h3>
 							</div>
 						);
 				  })}
