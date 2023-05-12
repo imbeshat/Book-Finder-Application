8f027c783543c6144d7b73c3e743232cf6f459d5
Imbeshat <md.imbeshat@gmail.com>
2023-05-12 22:44:11 +0530

    modified main component

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,10 +5,9 @@ const Main = () => {
 	return (
 		<main className="main-content flex flex-col justify-center items-center text-white">
 			<h1 className="text-5xl md:text-6xl lg:text-7xl text-center mb-4">Find Your Book</h1>
-			<p className="w-[80%] md:w-[600px]">
-				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis velit debitis quod ipsam accusamus voluptatum iusto aspernatur fuga suscipit
-				officiis rerum corrupti, dolorum hic sequi eius iste eum molestias, ipsum error soluta maiores perferendis sed mollitia amet. Illum impedit
-				unde possimus deleniti delectus consequuntur, doloremque minima fugiat recusandae beatae id corrupti distinctio quidem quis ipsa assumenda
+			<p className="w-[80%] md:w-[600px] md:text-lg lg:text-xl">
+				BooksHub is a place where you can find any type books and also you can know more about these books. BooksHub contains a whole lot of books.
+				Name a book and you will get that. Just type the name of the book which you want to view and you will get that.
 			</p>
 		</main>
 	);
