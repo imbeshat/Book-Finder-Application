a3c5bcd189f17b6f5bd8762bb3c2b679e95d0739
Imbeshat <md.imbeshat@gmail.com>
2023-05-15 14:30:29 +0530

    fixed BookItem design

--- src/components/BookItem.js
+++ src/components/BookItem.js
@@ -10,13 +10,13 @@ const BookItem = ({ data }) => {
 				? "Not Found"
 				: data.map((item) => {
 						return (
-							<div key={item.id} className="w-auto rounded-2xl bg-[#37264e] p-2 mb-4">
+							<div key={item.id} className="w-auto rounded-2xl bg-[#37264e] p-2 mb-4 flex flex-col justify-center items-center">
 								{item.volumeInfo && item.volumeInfo.imageLinks && item.volumeInfo.imageLinks.thumbnail ? (
 									<img src={item.volumeInfo.imageLinks.thumbnail} alt="book-cover" className="rounded-2xl" width={128} height={201} />
 								) : (
 									<img src={altCover} alt="book-cover" className="rounded-2xl" width={128} height={201} />
 								)}
-								<h3 className="text-white text-lg mt-2 mb-2">{item.volumeInfo && item.volumeInfo.title}</h3>
+								<h3 className="text-white text-lg mt-2 mb-2 text-center">{item.volumeInfo && item.volumeInfo.title}</h3>
 								<BookDetails data={item} />
 							</div>
 						);
